6db5325929e66e509012ae895195736345219a57
UltimativeWizard <44732036+UltimativeWizard@users.noreply.github.com>
2018-12-27 18:26:24 +0100

    Create Taschenrechner.java

---
+++ Taschenrechner.java
@@ -0,0 +1,13 @@
+package taschenrechner;
+
+
+public class Taschenrechner {
+
+  
+    public static void main(String[] args) {
+        TaschenrechneForm form = new TaschenrechneForm();
+        form.setVisible(true);
+        
+    }
+    
+}
